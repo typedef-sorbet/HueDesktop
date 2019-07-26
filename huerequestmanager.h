3d61cd5a8476770b2d29155a6558621a98987acc
Spencer Warneke <spencer.warneke@wsu.edu>
2019-07-26 14:30:46 -0700

    Button on GUI can now change Hue lights.
    Things to note:
    	- The HueRequestManager is intended to be a singleton, but I'm not sure how to enforce that in QML.
    	- HueRequestManager::changeLights does *not* check to ensure that the requests were successful; it is assumed that they are. I realize this is poor practice, but all attempts I have made so far to check that they succeed have resulted in unbuildable code or segfaulting applications. For now, I'm moving forward and leaving this issue for the future.

---
+++ huerequestmanager.h
@@ -0,0 +1,103 @@
+#ifndef HUEREQUESTMANAGER_H
+#define HUEREQUESTMANAGER_H
+
+#include <QObject>
+#include <QQmlApplicationEngine>
+#include <cmath>
+#include <QNetworkAccessManager>
+#include <QHttpMultiPart>
+#include <QHttpPart>
+#include <QNetworkRequest>
+#include <sstream>
+#include <QJsonObject>
+#include <QJsonArray>
+#include <QJsonDocument>
+#include <QNetworkReply>
+#include <QThread>
+
+class HueRequestManager : public QObject {
+    Q_OBJECT
+protected:
+    QNetworkAccessManager *mgr;
+
+    // RGB to Hue XY code adapted from tim on StackOverflow (accepted answer at https://stackoverflow.com/questions/22564187/rgb-to-philips-hue-hsb)
+    std::vector<double> *getRGBtoXY(float cred, float cgreen, float cblue)
+    {
+        // For the hue bulb the corners of the triangle are:
+        // -Red: 0.675, 0.322
+        // -Green: 0.4091, 0.518
+        // -Blue: 0.167, 0.04
+        double normalizedToOne[3];
+        normalizedToOne[0] = (cred / 255);
+        normalizedToOne[1] = (cgreen / 255);
+        normalizedToOne[2] = (cblue / 255);
+        float red, green, blue;
+
+        // Make red more vivid
+        if (normalizedToOne[0] > 0.04045) {
+            red = (float) pow(
+                    (normalizedToOne[0] + 0.055) / (1.0 + 0.055), 2.4);
+        } else {
+            red = (float) (normalizedToOne[0] / 12.92);
+        }
+
+        // Make green more vivid
+        if (normalizedToOne[1] > 0.04045) {
+            green = (float) pow((normalizedToOne[1] + 0.055)
+                    / (1.0 + 0.055), 2.4);
+        } else {
+            green = (float) (normalizedToOne[1] / 12.92);
+        }
+
+        // Make blue more vivid
+        if (normalizedToOne[2] > 0.04045) {
+            blue = (float) pow((normalizedToOne[2] + 0.055)
+                    / (1.0 + 0.055), 2.4);
+        } else {
+            blue = (float) (normalizedToOne[2] / 12.92);
+        }
+
+        float X = (float) (red * 0.649926 + green * 0.103455 + blue * 0.197109);
+        float Y = (float) (red * 0.234327 + green * 0.743075 + blue * 0.022598);
+        float Z = (float) (red * 0.0000000 + green * 0.053077 + blue * 1.035763);
+
+        float x = X / (X + Y + Z);
+        float y = Y / (X + Y + Z);
+
+        std::vector<double> *xyAsList = new std::vector<double>;
+        xyAsList->push_back(x);
+        xyAsList->push_back(y);
+        return xyAsList;
+    }
+public:
+    explicit HueRequestManager(QObject *parent = 0) : QObject(parent) {
+        mgr = new QNetworkAccessManager(this);
+    }
+
+    Q_INVOKABLE void changeLights(float r, float g, float b)
+    {
+        // TODO scale for multiple lights? I don't exactly intend for this to go public, so I wouldn't need to do this for a while, if at all...
+        std::vector<double> *xy = this->getRGBtoXY(r, g, b);
+        double x = xy->at(0), y = xy->at(1);
+
+        QUrl url_1("http://192.168.0.45/api/bqdaMSCscyVhBgZhkrF5ptFm2-NhJSAAg3rVmskl/lights/1/state");
+        QUrl url_2("http://192.168.0.45/api/bqdaMSCscyVhBgZhkrF5ptFm2-NhJSAAg3rVmskl/lights/2/state");
+
+        QNetworkRequest req_1, req_2;
+
+        std::ostringstream data_stream;
+        data_stream << "{\"xy\": [" << x << ", " << y << "]}";
+
+        req_1.setUrl(url_1);
+        req_1.setHeader(QNetworkRequest::ContentTypeHeader, QVariant("application/json"));
+        req_2.setUrl(url_2);
+        req_2.setHeader(QNetworkRequest::ContentTypeHeader, QVariant("application/json"));
+
+        QByteArray data(data_stream.str().c_str());
+
+        mgr->put(req_1, data);
+        mgr->put(req_2, data);
+    }
+};
+
+#endif // HUEREQUESTMANAGER_H
--- main.qml
+++ main.qml
@@ -1,9 +1,25 @@
 import QtQuick 2.11
 import QtQuick.Window 2.11
+import QtQuick.Controls 2.3
+import com.sanctity 1.0
 
 Window {
     visible: true
     width: 640
     height: 480
     title: qsTr("Hello World")
+
+    HueRequestManager {
+        id: manager
+    }
+
+    RoundButton {
+        text: "Change"
+
+        anchors.centerIn: parent
+
+        onPressed: {
+            manager.changeLights(255, 0, 0);
+        }
+    }
 }
